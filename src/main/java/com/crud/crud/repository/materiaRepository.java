004eafa3e45e0037dfbe6609ca2373d04cc0b950
JeroQ <aquiejc1@miumg.edu.gt>
2024-05-04 11:06:22 -0600

    nueva entidad

---
+++ src/main/java/com/crud/crud/repository/materiaRepository.java
@@ -0,0 +1,7 @@
+package com.crud.crud.repository;
+
+import com.crud.crud.model.materia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface materiaRepository extends JpaRepository<materia,Long> {
+}
